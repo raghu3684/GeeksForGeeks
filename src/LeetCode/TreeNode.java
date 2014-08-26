fac6111ed56d0ce4e11c54d11785d633070ae845
ggorantla <ggorantl@eng.ucsd.edu>
2014-08-25 17:46:12 -0700

    Committing the working codes of LeetCode

---
+++ src/LeetCode/TreeNode.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+/**
+ * Created by ggorantla on 8/22/2014.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x){
+        this.val = x;
+        this.left = null;
+        this.right = null;
+    }
+}
